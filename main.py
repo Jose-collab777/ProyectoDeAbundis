5857282563ff77bb61a390570ae3dec34025beb2
Jose-collab777 <ja772766@gmail.com>
2026-06-10 21:53:48 -0700

    PARA ENTREGAR YAgit add .!

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from libros import cargar_mis_libros
 from recomendaciones import Recomendador
 
 def iniciar_menu():
-    print("=" * 60)
     print("¡BIENVENIDO AL SISTEMA DE RECOMENDACIÓN DE LIBROS!")
+    print("Este programa analiza de forma automática una biblioteca utilizando")
+    print("frecuencia de palabras y la rareza de términos para extraer conceptos")
+    print("clave y recomendarte lecturas similares basadas en tus gustos.")
     print("=" * 60)
-    print("Cargando los libros, por favor espera...")
+    print("\nCargando los libros, por favor espera")
 
     # Mandamos llamar al módulo libros
     mis_libros = cargar_mis_libros()
@@ -17,10 +19,10 @@ def iniciar_menu():
     rec = Recomendador(mis_libros)
     rec.set_pesos()
 
-    print(f"¡Listo! Se procesaron correctamente {len(mis_libros)} libros.\n")
+    print(f" Se procesaron correctamente {len(mis_libros)} libros.\n")
 
     while True:
-        print("¿Qué te gustaría hacer hoy?")
+        print("¿Qué te gustaría hacer?")
         print("1. Ver el resumen de palabras clave de un libro")
         print("2. Recibir recomendaciones basadas en un libro que te guste")
         print("3. Salir del programa")
@@ -28,7 +30,7 @@ def iniciar_menu():
         opcion = input("\nSelecciona una opción (1, 2 o 3): ").strip()
 
         if opcion == "3":
-            print("\n¡Gracias por usar el recomendador! Vuelve pronto.")
+            print("\nGracias por usar el recomendador.")
             break
 
         if opcion in ["1", "2"]:
@@ -39,35 +41,35 @@ def iniciar_menu():
             print("-" * 40)
 
             try:
-                idx = int(input("\nIntroduce el ÍNDICE del libro: "))
+                idx = int(input("\nIntroduce el indice del libro: "))
                 if idx < 0 or idx >= len(mis_libros):
-                    print(" Error: Ese índice no existe.")
+                    print(" Error: Ese indice no existe.")
                     continue
             except ValueError:
-                print(" Error: Introduce un número válido.")
+                print(" Error: Introduce un numero válido.")
                 continue
 
             if opcion == "1":
                 try:
-                    num_palabras = int(input("¿Cuántas palabras clave deseas ver?: "))
+                    num_palabras = int(input("¿Cuantas palabras clave deseas ver? "))
                     palabras_clave = rec.resumen(idx, num_palabras)
                     print(f" PALABRAS CLAVE PARA: {mis_libros[idx].titulo.upper()}")
                     for i, palabra in enumerate(palabras_clave, 1):
                         print(f"  {i}. {palabra}")
                     print("=" * 40)
                 except ValueError:
-                    print(" Error: Debe ser un número entero.")
+                    print(" Error: Debe ser un numero entero.")
 
             elif opcion == "2":
                 try:
-                    num_recom = int(input("¿Cuántas recomendaciones deseas?: "))
+                    num_recom = int(input("¿Cuántas recomendaciones deseas? "))
                     libros_top = rec.libros_similares(idx, num_recom)
-                    print(f" LIBROS RECOMENDADOS BASADOS EN TU INTERÉS:")
+                    print(f" LIBROS RECOMENDADOS BASADOS EN TU INTERES:")
                     for i, libro in enumerate(libros_top, 1):
                         print(f"  {i}. {libro}")
                     print("=" * 40)
                 except ValueError:
-                    print("Error: Debe ser un número entero.")
+                    print("Error: Debe ser un numero entero.")
         else:
             print(" Opción no válida.")
 
